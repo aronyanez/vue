5478f344c5995570ad1a753fe3792b5d842ca44d
Arón <aronyanez@outlook.com>
2019-10-15 11:50:48 -0500

    INIT

---
+++ src/components/CardList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>lista de cards</h1>
+     Prueba: {{valor}}
+    <div v-for="contador in 20">
+      <span v-if="contador==5">Contador: {{contador}}</span>
+      <span v-else-if="contador==1">ELSE IF: {{contador}}</span>
+      <span v-else>Otro numero: {{contador}}</span>
+    </div>
+
+    <card-twitter v-for="item in 20" :key="item"></card-twitter>
+  </div>
+</template>
+
+<script>
+import CardTwitter from "@/components/CardTwitter.vue";
+export default {
+  components: {
+    CardTwitter
+  },
+
+  data() {
+    return {
+      valor: "prueba"
+    };
+  },
+
+  beforeCreate() {
+    console.log("Before create");
+  },
+  created() {
+    console.log("Iniciar");
+    this.valor = 100;
+  },
+  beforeMount() {
+      console.log("antes de mostrar dom");
+  },
+  mounted() {
+     console.log("montado"); 
+  },
+};
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/CardTwitter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+     Card
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    
+    };
+  },
+
+  methods: {
+   
+  },
+
+  computed: {
+    
+  }
+};
+</script>
+
+
+<style lang="css">
+.card{
+    width: 100px;
+    height: 100px;
+    background-color: green;
+
+}
+</style>
